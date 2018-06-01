d248f7b69b0ab840c9d970a639246e4577618e16
athuljayaraj <ME1@github>
2018-06-01 18:55:11 +0530

    Minor refactor

--- runtime/transform/joiner.py
+++ runtime/transform/joiner.py
@@ -10,8 +10,8 @@ def concat(list_of_joiner_input):
 def join(list_of_joiner_input, style):
     df = None
     if style == 'join':
-        df = reduce(lambda left, right: pd.merge(left.get('csv'), right.get('csv'), left_on=left.get('column'),
-                                                       right_on=right.get('column')), list_of_joiner_input)
+        df = reduce(lambda left, right: pd.merge(left.get('csv'), right.get('csv'), left_on=left.get('key'),
+                                                       right_on=right.get('key'),how='inner'), list_of_joiner_input)
     return df
 
 
@@ -20,6 +20,6 @@ import numpy as np
 
 if __name__ == '__main__':
     df1 = pd.DataFrame({'a': [1, 2, 3], 'b': [2, 3, 4]})
-    df2 = pd.DataFrame({'a': [1, 2, 3], 'c': [4, 7, 9]})
-    dt = join([{"csv":df1,"key":'a',"columns":"1,2,3"}, {"csv":df2,"key":'a',"columns":"1,2,3"}],'join')
+    df2 = pd.DataFrame({'d': [1, 2, 0], 'c': [4, 7, 9]})
+    dt = join([{"csv":df1,"key":'a',"columns":"1,2,3"}, {"csv":df2,"key":'d',"columns":"1,2,3"}],'join')
     print(dt)
