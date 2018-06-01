0142685b5b3d3810cef70c790ac7c28b60ad34f6
athuljayaraj <ME1@github>
2018-06-01 19:20:04 +0530

    Minor refactor

--- runtime/transform/joiner.py
+++ runtime/transform/joiner.py
@@ -2,17 +2,17 @@ from functools import reduce
 import pandas as pd
 import numpy as np
 
-def concat(list_of_joiner_input):
-    for joiner_input in list_of_joiner_input:
-        joiner_input.get('csv')
-
-
-def join(list_of_joiner_input, style):
+def concat(list_of_joiner_input,style):
     df = None
-    if style == 'join':
-        df = reduce(lambda left, right: pd.merge(left.get('csv'), right.get('csv'), left_on=left.get('key'),
-                                                       right_on=right.get('key'),how='inner'), list_of_joiner_input)
-    return df
+    # reduce(list_of_joiner_input)pd.concat([df_a, df_b], axis=1)
+
+
+def join(list_of_joiner_input):
+    left = list_of_joiner_input[0]
+
+    for right in list_of_joiner_input[1:]:
+        left['csv'] = pd.merge(left.get('csv'),right.get('csv'),left_on=left.get('key'),right_on=right.get('key'),how='inner')
+    return left['csv']
 
 
 import pandas as pd
@@ -21,5 +21,9 @@ import numpy as np
 if __name__ == '__main__':
     df1 = pd.DataFrame({'a': [1, 2, 3], 'b': [2, 3, 4]})
     df2 = pd.DataFrame({'d': [1, 2, 0], 'c': [4, 7, 9]})
-    dt = join([{"csv":df1,"key":'a',"columns":"1,2,3"}, {"csv":df2,"key":'d',"columns":"1,2,3"}],'join')
+    df3 = pd.DataFrame({'a': [2, 3, 0], 'e': [4, 7, 11]})
+    dt1 = pd.merge(df1,df2,left_on='a',right_on='d',how='inner')
+    dt2 = pd.merge(dt1, df3, left_on='a', right_on='a', how='inner')
+    dt = join([{"csv":df1,"key":'a',"columns":"1,2,3"}, {"csv":df2,"key":'d',"columns":"1,2,3"}, {"csv":df3,"key":'a',"columns":"1,2,3"}])
+    print(dt2)
     print(dt)
